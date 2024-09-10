d1c0112f003a58c3388b24474ea3ec6bc24869d3
Max Beligné <max.beligne@univ-grenoble-alpes.fr>
2024-09-10 14:58:49 +0200

    test change login

--- mon_site_use_proxy/mon_site/urls.py
+++ mon_site_use_proxy/mon_site/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import home, login
+import django_cas_ng.views
 
 urlpatterns = [
     path('', home, name='home'),
-    path('accounts/login', login , name='cas_ng_login'),
+    path('accounts/login', django_cas_ng.views.LoginView.as_view(), name='cas_ng_login'),
     ]
