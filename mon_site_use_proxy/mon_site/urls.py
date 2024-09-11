905c6139fd891e8aa88ff09fd54703275cbfa8e0
Max Beligné <max.beligne@univ-grenoble-alpes.fr>
2024-09-11 19:41:42 +0200

    add logout

--- mon_site_use_proxy/mon_site/urls.py
+++ mon_site_use_proxy/mon_site/urls.py
@@ -5,4 +5,5 @@ import django_cas_ng.views
 urlpatterns = [
     path('', home, name='home'),
     path('accounts/login', django_cas_ng.views.LoginView.as_view(), name='cas_ng_login'),
+    path('accounts/logout', django_cas_ng.views.LogoutView.as_view(), name='cas_ng_logout'),
     ]
